b1770b4920b46a41635aeef8a2be4b52dfea03d8
Aryan Mishra <aryanmishracoc@gmail.com>
2023-01-02 15:06:48 +0530

    Side Drawer added to parent app

--- BusyBeeParent/app/src/main/java/poize/busybee_parent/MainActivity.java
+++ BusyBeeParent/app/src/main/java/poize/busybee_parent/MainActivity.java
@@ -1,14 +1,67 @@
 package poize.busybee_parent;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navView_HomeScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_homeScreen);
+        navView_HomeScreen = findViewById(R.id.navView_homeScreen);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView_HomeScreen.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fragmentSwitch(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_navigation, fragment).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_account:{
+                //TODO: fragmentswitch to Account FRAGMENT
+            }
+            case R.id.nav_logout:{
+                fragmentSwitch(new LoginFragment());
+                drawerLayout.close();
+                //TODO: remove from shared pref
+            }
+
+        }
+        return false;
     }
 }
