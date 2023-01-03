fd2e9df5a22482d8be6a724bf6a8b5559035be4f
ayush27prasad <ayushprasadcool@gmail.com>
2023-01-04 03:21:04 +0530

    added grid layout in games fragg.

--- BusyBeeChild/app/src/main/java/poize/busybee_child/GameFragment.java
+++ BusyBeeChild/app/src/main/java/poize/busybee_child/GameFragment.java
@@ -59,6 +59,15 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+
+
+
+
+        return view;
+
+
+
+
     }
 }
